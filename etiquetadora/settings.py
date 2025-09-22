a4b851defad2bd2dd6e47cb2b8216ff37b8502da
WebNeocat <webneocat@gmail.com>
2025-09-22 12:51:15 +0200

    settings

--- etiquetadora/settings.py
+++ etiquetadora/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback_temporal')
 DEBUG = False
 
 # Cambia esto al dominio que Render te asigna
-ALLOWED_HOSTS = ['cheftag.onrender.com']  # <- reemplaza con tu dominio Render
+ALLOWED_HOSTS = ['cheftag.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
@@ -29,9 +29,10 @@ INSTALLED_APPS = [
     'app.super',
 ]
 
+# Middleware con WhiteNoise para servir staticfiles en producción
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <- agregado
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -110,10 +111,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"  # <- necesario para producción
+
+# Carpeta donde collectstatic recopila todos los archivos
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# WhiteNoise Storage para servir archivos estáticos comprimidos y cacheados
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
 MEDIA_URL = '/media/'
@@ -123,4 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Seguridad extra
-CSRF_TRUSTED_ORIGINS = ['https://cheftag.onrender.com']  # <- reemplaza con tu dominio
+CSRF_TRUSTED_ORIGINS = ['https://cheftag.onrender.com']
+
